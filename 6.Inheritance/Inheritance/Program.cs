2b5d318c28ad94cce6d52ab1a6c1e1d93480d2b6
Breno Quintino <brenoqcarvalho@gmail.com>
2020-07-01 14:45:52 -0300

    Novas aulas inseridas

---
+++ 6.Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    //Here a example of a program like PowerPoint
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
---
+++ Composition/Composition/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composition
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            if (message != null) Console.WriteLine(message);
+        }
+    }
+}
